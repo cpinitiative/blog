8c97e6567be52295248fd9bbc7aca6b8b791c3b7
Nathan Wang <nathan.r.wang@gmail.com>
2021-08-30 05:35:15 +0000

    adjust theme color

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
         sans: ['Inter', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        primary: colors.teal,
+        primary: colors.fuchsia,
         gray: colors.trueGray,
         code: {
           green: '#b5f4a5',
